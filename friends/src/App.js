feeba30bcd79b5ce051fe7c829bd8f5c5fbef0dd
Joshua Edgerton <joshuaxedgerton@gmail.com>
2019-11-12 15:14:34 -0600

    Basic structure and route added

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from "react-router-dom"
+import { PrivateRoute } from "./components/PrivateRoute"
+import Login from "./components/Login"
+import FriendsList from "./components/FriendsList"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Switch>
+       <Route path="/login" component={Login} />
+       <PrivateRoute path="/friendslist" component={FriendsList} />
+       <Route component={Login} />
+     </Switch>
     </div>
   );
 }
